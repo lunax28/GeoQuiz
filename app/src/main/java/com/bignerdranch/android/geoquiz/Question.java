c162d14b2860943ff2ef43d69f1b8b4c4504916a
lunax <albertofly10@gmail.com>
2017-10-14 15:27:39 +0200

    Initial Commit

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
@@ -0,0 +1,34 @@
+package com.bignerdranch.android.geoquiz;
+
+/**
+ * Created by albus on 14-Oct-17.
+ */
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public int getTextResId() {
+
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+
+    public Question(int mTextResId, boolean mAnswerTrue) {
+
+        this.mTextResId = mTextResId;
+        this.mAnswerTrue = mAnswerTrue;
+    }
+}
